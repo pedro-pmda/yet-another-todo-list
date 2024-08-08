c39e251d177a281e5fe64fef8c590705dd5e780d
Pedro Palazón Martínez de Alegría <pedro.palazon@zooplus.com>
2024-08-08 14:37:32 +0200

    feat: add header

---
+++ src/AppHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1><span class="icon" aria-hidden="true">✅</span> To do</h1>
+    <p>Building Real world Web Application with Vue.js 3</p>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+header {
+  border-bottom: #333 1px solid;
+  background-color: #fff;
+}
+
+header::after {
+  content: '';
+  display: block;
+  height: 1px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
+}
+
+h1 {
+  font-size: 2remn;
+}
+
+h1 .icon {
+  font-size: 1rem;
+  vertical-align: middle;
+}
+</style>

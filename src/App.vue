449b1163b628f306320c30d2f9b8b6fed842d640
Pedro Palazón Martínez de Alegría <pedro.palazon@zooplus.com>
2024-08-08 15:43:37 +0200

    feat: add favicon and create mock list

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppHeader from '@/components/AppHeader.vue'
+import TodList from '@/components/TodoList.vue'
+</script>
 
 <template>
-  <header>
-    <div class="wrapper"></div>
-  </header>
+  <div id="app">
+    <header>
+      <AppHeader />
+    </header>
 
-  <main>
-    <h1>YET ANOTHER TODO LIST</h1>
-  </main>
+    <main>
+      <TodList />
+    </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style></style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import ListItem from '@/components/ListItem.vue'
+
+interface Item {
+  title: string
+  checked: boolean
+}
+
+const listItems: Item[] = [
+  { title: 'Clean the kitchen', checked: true },
+  { title: 'Do the laundry', checked: false },
+  { title: 'Vacuum the living room', checked: false },
+  { title: 'Water the plants', checked: true },
+  { title: 'Organize the closet', checked: false },
+  { title: 'Take out the trash', checked: true },
+  { title: 'Mow the lawn', checked: false },
+  { title: 'Wash the dishes', checked: false },
+  { title: 'Prepare meals for the week', checked: false },
+  { title: 'Change bed sheets', checked: true }
+]
+</script>
+
+<template>
+  <ul>
+    <li :key v-for="(item, key) in listItems">
+      <ListItem :is-checked="false">{{ item.title }}</ListItem>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  list-style: none;
+}
+
+li {
+  margin: 0.4rem 0;
+}
+</style>

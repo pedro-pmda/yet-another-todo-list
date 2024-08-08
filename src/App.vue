13f54aee0a3ec5f08f614cba26e7f3be95300d9d
Pedro Palazón Martínez de Alegría <pedro.palazon@zooplus.com>
2024-08-08 11:33:46 +0200

    chore: scaffold

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <div class="wrapper"></div>
+  </header>
+
+  <main>
+    <h1>YET ANOTHER TODO LIST</h1>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
